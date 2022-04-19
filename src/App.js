9040dd93c5688152fd9457f50e1d8a3736896c30
suochantsao <suochantsao@gmail.com>
2022-04-19 17:31:27 +0800

    Rewrite render function's statement

--- src/App.js
+++ src/App.js
@@ -10,15 +10,28 @@ class App extends React.Component {
     // to this.state
     this.state = { lat: null };
 
-    window.navigator.geolocation.getCurrentPosition((position) => {
-      // we called setState function here
-      this.setState({ lat: position.coords.latitude });
-    });
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState function here
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
   }
 
   // React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
